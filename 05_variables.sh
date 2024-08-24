455d7c0c806ec231fec4f9b01c0e372d9b8a6b0e
Chinnapareddy Mora <chinnapareddy.mora@wisseninfotech.com>
2024-08-24 09:58:01 +0530

    pushing new variables

---
+++ 05_variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your username ::"
+
+read -s USERNAME
+
+echo "username entered is:"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "password entered is:"
